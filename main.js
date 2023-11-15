3452b41343cf7ec30f6694a2cae6b79ec7d7e1f0
Git-KLW <124479692+Git-KLW@users.noreply.github.com>
2023-11-15 13:34:41 -0600

    Created Main File
    Created the main inclusion file for the quark engine project.

---
+++ main.js
@@ -0,0 +1,41 @@
+class Game {
+  constructor() {
+    this.colliders = [];
+  };
+};
+var game = new Game();
+class Vector2 {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  };
+};
+class RigidBody2D {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.direction = 0;
+    this.velocity = {"xv":0, "yv":0};
+    this.gravity = null;
+    this.collider = null;
+  };
+};
+// TODO: Add Softbodies
+class Collider {
+  constructor(x, y, width, height) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.state = null;
+    game.Colliders.push(this);
+  };
+};
+class BoxCollider extends Collider {
+  constructor() {
+    game.Colliders.forEach((collider) => {
+      
+    });
+  };
+};
+// TODO: Add Circlar Colliders
